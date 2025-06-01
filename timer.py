7e597fded23f54b82ade9dfd830978c5f943e186
Jang hyun woo <jhwunoo@naver.com>
2025-06-01 16:01:10 +0900

    time.

---
+++ timer.py
@@ -0,0 +1,36 @@
+import time
+
+class Timer:
+    def __init__(self, timeInSeconds, nickname=None, callBack=None):
+        self.timeInSeconds = timeInSeconds
+        self.nickname = nickname
+        self.callBack = callBack
+        self.savedSecondsElapsed = 0.0
+        self.running = False
+
+    def start(self, newTimeSeconds=None):
+        if newTimeSeconds is not None:
+            self.timeInSeconds = newTimeSeconds
+        self.running = True
+        self.startTime = time.time()
+
+    def update(self):
+        if not self.running:
+            return False
+        self.savedSecondsElapsed = time.time() - self.startTime
+        if self.savedSecondsElapsed < self.timeInSeconds:
+            return False
+        else:
+            self.running = False
+            if self.callBack is not None:
+                self.callBack(self.nickname)
+            return True
+
+    def getTime(self):
+        if self.running:
+            self.savedSecondsElapsed = time.time() - self.startTime
+        return self.savedSecondsElapsed
+
+    def stop(self):
+        self.getTime()
+        self.running = False
